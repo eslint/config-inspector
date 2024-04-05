069d217a0707b2de802b6f7e727436fb8f11a357
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-05 14:04:17 +0200

    chore: update

--- app/server/api/payload.json.ts
+++ app/server/api/payload.json.ts
@@ -1,7 +1,10 @@
+import process from 'node:process'
 import { createWsServer } from '~~/src/ws'
 
 export default lazyEventHandler(async () => {
-  const ws = await createWsServer()
+  const ws = await createWsServer({
+    cwd: process.cwd(),
+  })
 
   return defineEventHandler(async () => {
     return await ws.getData()
