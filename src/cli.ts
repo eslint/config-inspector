09e55aabe77d6d4697ab9c4d657a40cf7f082a46
yuyinws <lyc657508366@gmail.com>
2024-04-06 23:31:53 +0800

    refactor: use `h3` to create server

--- src/cli.ts
+++ src/cli.ts
@@ -2,9 +2,11 @@ import process from 'node:process'
 import fs from 'node:fs/promises'
 import { existsSync } from 'node:fs'
 import { relative, resolve } from 'node:path'
+import { createServer } from 'node:http'
 import open from 'open'
 import { getPort } from 'get-port-please'
 import cac from 'cac'
+import { toNodeListener } from 'h3'
 import { createHostServer } from './server'
 import { distDir } from './dirs'
 import { readConfig } from './configs'
@@ -67,7 +69,7 @@ cli
       userBasePath: options.basePath,
     })
 
-    server.listen(port, host)
+    createServer(toNodeListener(server)).listen(port, host)
 
     if (options.open)
       await open(`http://${host === '127.0.0.1' ? 'localhost' : host}:${port}`)
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,40 @@
-import { createServer } from 'node:http'
-import connect from 'connect'
-import sirv from 'sirv'
+import { join } from 'node:path'
+import { readFile, stat } from 'node:fs/promises'
+import { createApp, eventHandler, serveStatic } from 'h3'
+import { lookup } from 'mrmime'
 import { type CreateWsServerOptions, createWsServer } from './ws'
 import { distDir } from './dirs'
 
 export async function createHostServer(options: CreateWsServerOptions) {
-  const app = connect()
+  const app = createApp()
 
   const ws = await createWsServer(options)
 
-  app.use('/api/payload.json', async (_req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(await ws.getData()))
-  })
-
-  app.use(sirv(distDir, {
-    dev: true,
-    single: true,
+  app.use('/api/payload.json', eventHandler(async (event) => {
+    event.node.res.setHeader('Content-Type', 'application/json')
+    return event.node.res.end(JSON.stringify(await ws.getData()))
   }))
 
-  return createServer(app)
+  app.use('/', eventHandler(async (event) => {
+    const result = await serveStatic(event, {
+      fallthrough: true,
+      getContents: id => readFile(join(distDir, id), 'utf-8'),
+      getMeta: async (id) => {
+        const stats = await stat(join(distDir, id)).catch(() => {})
+        if (!stats || !stats.isFile())
+          return
+
+        return {
+          type: lookup(id),
+          size: stats.size,
+          mtime: stats.mtimeMs,
+        }
+      },
+    })
+
+    if (result === false)
+      return readFile(join(distDir, 'index.html'), 'utf8')
+  }))
+
+  return app
 }
