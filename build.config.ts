179bc643871c97e4c57e423ee94d5b52d8c3f675
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-22 14:05:32 +0200

    fix: patch bundle-require
    https://github.com/egoist/bundle-require/pull/41
    https://github.com/nuxt/eslint/issues/400

--- build.config.ts
+++ build.config.ts
@@ -5,4 +5,8 @@ export default defineBuildConfig({
     'src/cli.ts',
   ],
   clean: false,
+  // TODO: remove this when https://github.com/egoist/bundle-require/pull/41 is merged
+  rollup: {
+    inlineDependencies: true,
+  },
 })
---
+++ patches/README.md
@@ -0,0 +1,1 @@
+- Remove the `bundle-require` patch and move it to dependencies after https://github.com/egoist/bundle-require/pull/41 get merged
