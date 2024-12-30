e49cb7462b59b312b332570e4efb76f23cedd222
Anthony Fu <github@antfu.me>
2024-12-30 11:45:25 +0800

    chore: migrate to Vitest

--- tests/shared_configs.test.ts
+++ tests/shared_configs.test.ts
@@ -1,5 +1,5 @@
-import assert from 'node:assert'
-import { matchFile } from '../shared/configs.js'
+import { describe, expect, it } from 'vitest'
+import { matchFile } from '../shared/configs'
 
 describe('matchFile', () => {
   describe('global ignored', () => {
@@ -9,7 +9,7 @@ describe('matchFile', () => {
     }
     it('should match no configs', () => {
       const result = testGlobalIgnores(['tests/**'])
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['tests/**'],
         configs: [],
@@ -18,7 +18,7 @@ describe('matchFile', () => {
 
     it('should match when final matched glob is not an unignore', () => {
       const result = testGlobalIgnores(['tests/**', '!tests/folder/**', 'tests/**/*.test.ts'])
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['tests/**', '!tests/folder/**', 'tests/**/*.test.ts'],
         configs: [],
@@ -27,7 +27,7 @@ describe('matchFile', () => {
 
     it('should match when irrelevant unignores included', () => {
       const result = testGlobalIgnores(['tests/**', '!tests/other/**', '!tests/*.test.ts'])
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['tests/**'],
         configs: [],
@@ -36,7 +36,7 @@ describe('matchFile', () => {
 
     it('should match when file is unignored but directory is ignored', () => {
       const result = testGlobalIgnores(['tests/**', '!tests/**/*.test.ts', 'tests/other/**'])
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['tests/**', '!tests/**/*.test.ts'],
         configs: [],
@@ -45,7 +45,7 @@ describe('matchFile', () => {
 
     it('should not match when file is unignored and directory is ignored but sub directory is unignored', () => {
       const result = testGlobalIgnores(['tests/**/*', '!tests/**/*/', '!tests/**/*.test.ts', 'tests/other/**'])
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['tests/folder/foo.test.ts', 'tests/**/*', '!tests/**/*.test.ts'],
         configs: [0],
@@ -56,7 +56,7 @@ describe('matchFile', () => {
   describe('config matching', () => {
     it('should match a basic config', () => {
       const result = matchFile('tests/folder/foo.test.ts', [{ index: 0, files: ['**'], ignores: [] }], process.cwd())
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['**'],
         configs: [0],
@@ -65,7 +65,7 @@ describe('matchFile', () => {
 
     it('should be ignored when final matched glob is not an unignore', () => {
       const result = matchFile('tests/folder/foo.test.ts', [{ index: 0, files: ['**'], ignores: ['tests/**', '!tests/folder/**', 'tests/**/*.test.ts'] }], process.cwd())
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['tests/**', '!tests/folder/**', 'tests/**/*.test.ts'],
         configs: [],
@@ -74,7 +74,7 @@ describe('matchFile', () => {
 
     it('should match when last matching glob is an unignore', () => {
       const result = matchFile('tests/folder/foo.test.ts', [{ index: 0, files: ['**'], ignores: ['tests/**', '!tests/folder/**', 'tests/other/**'] }], process.cwd())
-      assert.deepEqual(result, {
+      expect(result).toEqual({
         filepath: 'tests/folder/foo.test.ts',
         globs: ['**', 'tests/**', '!tests/folder/**'],
         configs: [0],
@@ -92,7 +92,7 @@ describe('matchFile', () => {
         { index: 6, files: ['**'], ignores: ['tests/**', '!tests/*.test.ts', 'tests/folder/*'] },
         { index: 7, files: ['**'], ignores: ['tests/**', '!tests/*.test.ts', 'tests/folder/*', '!tests/folder/*.ts', 'tests/other/**'] },
       ], process.cwd())
-      assert.deepEqual(result.configs, [0, 3, 4, 7])
+      expect(result.configs).toEqual([0, 3, 4, 7])
     })
   })
 })
